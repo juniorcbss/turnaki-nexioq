661e6c4a6db4448902d66b1217b6c72a9bb1675c
Calixto Saldarriaga <junior.cbss@gmail.com>
2025-10-07 10:07:32 -0500

    fix: activar backend S3 para Terraform state
    - Inicializado backend S3: turnaki-nexioq-terraform-state
    - Inicializada tabla DynamoDB: turnaki-nexioq-terraform-locks
    - Descomentado backend en dev, qas, prd
    - Necesario para que funcionen los workflows terraform-plan

--- terraform/environments/prd/backend.tf
+++ terraform/environments/prd/backend.tf
@@ -1,11 +1,11 @@
 # Backend configuration para state remoto PRD
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "turnaki-nexioq-terraform-state"
-#     key            = "prd/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "turnaki-nexioq-terraform-locks"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "turnaki-nexioq-terraform-state"
+    key            = "prd/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "turnaki-nexioq-terraform-locks"
+  }
+}
